d690d657f618a351618553f78fb9303c139944fc
Johannes Wünsche <johannes.wuensche97@gmail.com>
2019-09-01 16:40:02 +0200

    Add documentation

--- src/bin/end2end_switch.rs
+++ src/bin/end2end_switch.rs
@@ -6,6 +6,6 @@ use cursive::Cursive;
 fn main() {
     let mut siv = Cursive::default();
     let mut tabs = TabView::new().with_view(0, TextView::new("First")).with_view(1, TextView::new("Second"));
-    tabs.set_tab(0);
+    tabs.set_tab(0).expect("Setting tab has failed");
     siv.run();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,42 @@
+//! This crate provides a tabbing view for [gyscos/cursive](https://github.com/gyscos/cursive) views. It is build to be as simple as possible.
+//!
+//! The behaviour is oriented at the [`StackView`](https://docs.rs/cursive/0.13.0/cursive/views/struct.StackView.html) of cursive, but with the advantage of selectively displaying
+//! views without needing to delete foremost one.
+//!
+//! # Example
+//! All you need to do to create a new `TabView` is:
+//! ```
+//! # use cursive::{Cursive, views::{TextView, Dialog}};
+//! # use cursive_tabs::TabView;
+//! # fn main() {
+//! #   let mut siv = Cursive::default();
+//! let mut tabs = TabView::new();
+//! #   // That is all what is needed to display an empty TabView, but of course
+//! #   // you can add your own tabs now and switch them around as you want!
+//! #   tabs.insert_view("First", TextView::new("Our first view!"));
+//! #   siv.add_layer(Dialog::around(tabs));
+//! #   // When your done setting run cursive
+//! #   // siv.run();
+//! # }
+//! ```
+//! You can then use the provided methods to modify the content of the `TabView`
+//! Consuming and non-consuming are both provided.
+//!
+//! # Full Example
+//! ```
+//! use cursive::{Cursive, views::{TextView, Dialog}};
+//! use cursive_tabs::TabView;
+//! fn main() {
+//!   let mut siv = Cursive::default();
+//!   let mut tabs = TabView::new();
+//!   // That is all what is needed to display an empty TabView, but of course
+//!   // you can add your own tabs now and switch them around as you want!
+//!   tabs.insert_view("First", TextView::new("Our first view!"));
+//!   siv.add_layer(Dialog::around(tabs));
+//!   // When your done setting run cursive
+//!   // siv.run();
+//! }
+//! ```
 use cursive::direction::Direction;
 use cursive::event::{AnyCb, Event, EventResult};
 use cursive::view::{Selector, View};
@@ -5,12 +44,29 @@ use cursive::{Printer, Rect, Vec2};
 use std::collections::HashMap;
 use std::hash::Hash;
 
+/// Main struct which manages views
 pub struct TabView<K: Hash> {
     current_id: Option<K>,
     map: HashMap<K, Box<dyn View>>,
 }
 
 impl<K: Hash + Eq + Copy + 'static> TabView<K> {
+    /// Returns a new TabView
+    /// # Example
+    /// ```
+    /// # use cursive::{Cursive, views::{TextView, Dialog}};
+    /// # use cursive_tabs::TabView;
+    /// # fn main() {
+    /// #  let mut siv = Cursive::default();
+    /// let mut tabs = TabView::new();
+    /// #  // That is all what is needed to display an empty TabView, but of course
+    /// #  // you can add your own tabs now and switch them around as you want!
+    /// #  tabs.insert_view("First", TextView::new("Our first view!"));
+    /// #  siv.add_layer(Dialog::around(tabs));
+    /// #  // When your done setting run cursive
+    /// #  // siv.run();
+    /// # }
+    /// ```
     pub fn new() -> Self {
         Self {
             current_id: None,
@@ -18,6 +74,8 @@ impl<K: Hash + Eq + Copy + 'static> TabView<K> {
         }
     }
 
+    /// Non-consuming setting of the to be active tab.
+    /// If the tab id is not known, an error is returned and no action is performed.
     pub fn set_tab(&mut self, id: K) -> Result<(), ()> {
         if self.map.contains_key(&id) {
             self.current_id = Some(id);
@@ -27,22 +85,29 @@ impl<K: Hash + Eq + Copy + 'static> TabView<K> {
         }
     }
 
+    /// Consuming insertion of a new view.
+    /// Active tab will be switched to the newly inserted one.
     pub fn with_view<T: View>(mut self, id: K, view: T) -> Self {
         self.map.insert(id, Box::new(view));
         self.current_id = Some(id);
         self
     }
 
+    /// Non-cosuming insertion of a new view.
+    /// Active tab will be switched to the newly inserted one.
     pub fn insert_view<T: View>(&mut self, id: K, view: T) -> K {
         self.map.insert(id, Box::new(view));
         self.current_id = Some(id);
         id
     }
 
+    /// Returns the currently active tab Id.
     pub fn tab(&self) -> Option<K> {
         self.current_id
     }
 
+    /// Removes the given id from the `TabView`.
+    /// If the removed view is active at the moment, the `TabView` will unfocus it and the focus needs to be set manually afterwards, or anew view has to be inserted.
     pub fn remove_view(&mut self, id: K) -> Result<K, ()> {
         if let Some(_) = self.map.remove(&id) {
             if let Some(key) = &self.current_id {
