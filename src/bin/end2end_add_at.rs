ba1e85d4f0e49778428e760f2d34485582330e48
Johannes Wünsche <johannes.wuensche97@gmail.com>
2019-12-03 17:28:23 +0100

    Update test to cover more cases

--- src/bin/end2end_add_at.rs
+++ src/bin/end2end_add_at.rs
@@ -6,8 +6,9 @@ use cursive::Cursive;
 fn main() {
     let mut siv = Cursive::default();
     let tabs = TabView::new()
-        .with_tab_at(0, TextView::new("Second"), 0)
-        .with_tab_at(1, TextView::new("First"), 0);
+        .with_tab_at(0, TextView::new("Third"), 0)
+        .with_tab_at(1, TextView::new("First"), 0)
+        .with_tab_at(2, TextView::new("Second"), 1);
     siv.add_layer(tabs);
     siv.run();
 }
--- src/bin/end2end_add_at_panel.rs
+++ src/bin/end2end_add_at_panel.rs
@@ -7,7 +7,8 @@ fn main() {
     let mut siv = Cursive::default();
     let tabs = TabPanel::new()
         .with_tab("Stonks", TextView::new("Pshhhh"))
-        .with_tab_at("Much", TextView::new("Ahhhhh"), 0)
+        .with_tab_at("So", TextView::new("Fooooo"), 0)
+        .with_tab_at("Much", TextView::new("Ahhhhh"), 1)
         .with_bar_alignment(HAlign::Center);
     siv.add_layer(tabs);
     siv.run();
