a015a621d93d58fe1bdec0b0276862393f7ef732
Johannes Wünsche <johannes.wuensche97@gmail.com>
2020-01-15 22:19:33 +0100

    Add tests for actions

--- src/bin/end2end_vertical_left.rs
+++ src/bin/end2end_vertical_left.rs
@@ -7,6 +7,7 @@ fn main() {
     let mut siv = Cursive::default();
     let tabs = TabPanel::new()
         .with_tab("Stronk test", TextView::new("Pshhhh"))
+        .with_tab("Stronker test", TextView::new("Pshhhh"))
         .with_active_tab("Stronk test")
         .expect("Setting active tab has failed")
         .with_bar_alignment(Align::Center)
--- tests/end2end.test.js
+++ tests/end2end.test.js
@@ -26,6 +26,14 @@ it('tests removal of a currently inactive tab', cargo_e2e('remove_inactive'));
 it('tests switching tabs', cargo_e2e('switch'));
 it('tests initialization and rendering of left placed bar', cargo_e2e('vertical_left'));
 it('tests initialization and rendering of right placed bar', cargo_e2e('vertical_right'));
+it('tests the sending of keys in vertical mode', cargo_e2e('vertical_left', async () => {
+    await expect.command('tmux send-keys Up && sleep 0.1')
+        .forExitCode(exp => exp.toBe(0));
+}));
+it('tests the sending of keys in vertical mode', cargo_e2e('add_at', async () => {
+    await expect.command('tmux send-keys Left && sleep 0.1')
+        .forExitCode(exp => exp.toBe(0));
+}));
 it('tests inserting tabs at defined position', cargo_e2e('add_at'));
 it('tests inserting tabs at defined position in panel', cargo_e2e('add_at_panel'));
 it('smoke tests the tab panel', cargo_e2e('panel_smoke'));
